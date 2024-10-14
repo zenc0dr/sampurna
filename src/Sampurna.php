44b6685aef9ef98d517b998a79dcf5520161d922
Alex Blaze <zen@8ber.ru>
2024-10-14 16:14:47 +0400

    Some fixes

---
+++ src/Sampurna.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Zenc0dr\Sampurna;
+
+class Sampurna
+{
+    /**
+     * Test function
+     * @return string
+     */
+    public static function test(): string
+    {
+        return 'Hallo Sampurna';
+    }
+}
