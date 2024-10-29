53c19d5a204c623030892baee8ed33401e8229e3
Alex Blaze <zen@8ber.ru>
2024-10-30 03:17:01 +0400

    Some fixes

--- tests/Unit/FlowTest.php
+++ tests/Unit/FlowTest.php
@@ -2,12 +2,33 @@
 
 namespace Zen\Sampura\Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class FlowTest extends TestCase
 {
+    private static string $test_stack_uuid;
+    private static string $test_stack_path;
+
+    protected function setUp(): void
+    {
+
+        self::$test_stack_uuid = 'sampurna_test_stack';
+        self::$test_stack_path = '/app/storage/sampurna_vault/stacks/' . self::$test_stack_uuid . '.json';
+
+        parent::setUp();
+        if (file_exists(self::$test_stack_path)) {
+            unlink(self::$test_stack_path);
+        }
+    }
+
     public function test_create_stack()
     {
-        $this->assertTrue(true);
+        sampurna()->stack(self::$test_stack_uuid)->create('Тестовый стэк');
+
+
+
+        $this->assertTrue(
+            file_exists(self::$test_stack_path)
+        );
     }
 }
